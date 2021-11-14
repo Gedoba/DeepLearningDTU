ae6f0701403d4db95044560f395bbf852bfc59cb
Wojciech Ciok <33331400+wojciechciok@users.noreply.github.com>
2021-11-14 13:34:54 +0100

    Update train.py

--- train.py
+++ train.py
@@ -7,14 +7,15 @@ from visualize import visualize
 class AverageMeter:
     def __init__(self):
         self.reset()
-        
+
     def reset(self):
         self.count, self.avg, self.sum = [0.] * 3
-    
+
     def update(self, val, count=1):
         self.count += count
         self.sum += count * val
         self.avg = self.sum / self.count
+
 
 def create_loss_meters() -> dict[str, AverageMeter]:
     loss_D_fake = AverageMeter()
@@ -23,7 +24,7 @@ def create_loss_meters() -> dict[str, AverageMeter]:
     loss_G_GAN = AverageMeter()
     loss_G_L1 = AverageMeter()
     loss_G = AverageMeter()
-    
+
     return {'loss_D_fake': loss_D_fake,
             'loss_D_real': loss_D_real,
             'loss_D': loss_D,
@@ -38,15 +39,14 @@ def update_losses(model, loss_meter_dict, count):
         loss_meter.update(loss.item(), count=count)
 
 
-
 def save_model(path, model: MainModel, epoch, loss_meter_dict: dict[str, AverageMeter]):
     torch.save({
-            'epoch': epoch,
-            'model_state_dict': model.state_dict(),
-            'optimizer_state_dict_G': model.opt_G.state_dict(),
-            'optimizer_state_dict_D': model.opt_D.state_dict(),
-            'losses': loss_meter_dict
-            }, path)
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict_G': model.opt_G.state_dict(),
+        'optimizer_state_dict_D': model.opt_D.state_dict(),
+        'losses': loss_meter_dict
+    }, path)
 
 
 def load_model(path, model):
@@ -61,30 +61,34 @@ def load_model(path, model):
     return model, loss_meter_dict
 
 
-
 def log_results(loss_meter_dict):
     for loss_name, loss_meter in loss_meter_dict.items():
         print(f"{loss_name}: {loss_meter.avg:.5f}")
 
 
-def train_model(model, train_dl, val_dl, color_space, epochs, display_every=200, loss_meter_dict = None, save_path = None):
-    data = next(iter(val_dl)) # getting a batch for visualizing the model output after fixed intrvals
+def train_model(model, train_dl, val_dl, color_space, epochs, display_every=200, loss_meter_dict=None, save_path=None):
+    # getting a batch for visualizing the model output after fixed intrvals
+    vis_data = next(iter(val_dl))
     starting_epoch = model.epoch
     for e in range(epochs):
-        # function returing a dictionary of objects to 
+        # function returing a dictionary of objects to
         # log the losses of the complete network
-        loss_meter_dict = create_loss_meters() if loss_meter_dict is None else loss_meter_dict 
-        i = 0                                  
+        loss_meter_dict = create_loss_meters() if loss_meter_dict is None else loss_meter_dict
+        i = 0
         for data in tqdm(train_dl):
-            model.setup_input(data) 
+            model.setup_input(data)
             model.optimize()
-            update_losses(model, loss_meter_dict, count=data['known_channel'].size(0)) # function updating the log objects
+            # function updating the log objects
+            update_losses(model, loss_meter_dict,
+                          count=data['known_channel'].size(0))
             i += 1
             if i % display_every == 0:
                 print(f"\nEpoch {model.epoch + 1}/{epochs + starting_epoch}")
                 print(f"Iteration {i}/{len(train_dl)}")
-                log_results(loss_meter_dict) # function to print out the losses
-                visualize(model, data, color_space, save=False) # function displaying the model's outputs
+                # function to print out the losses
+                log_results(loss_meter_dict)
+                # function displaying the model's outputs
+                visualize(model, vis_data, color_space, save=False)
         model.epoch += 1
         if save_path is not None:
             save_model(save_path, model, model.epoch, loss_meter_dict)
